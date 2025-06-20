bcf6b6ee190f9e58e8e825ed90c616ccf60dc608
AngeloZorzi <astro.drg666@gmail.com>
2025-06-20 10:55:55 +0200

    Initial Commit from terminal, Spring Boot Project

---
+++ src/main/java/it/epicode/S7L5_ProgettoSettimanale/S7L5ProgettoSettimanaleApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.S7L5_ProgettoSettimanale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S7L5ProgettoSettimanaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S7L5ProgettoSettimanaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/S7L5_ProgettoSettimanale/S7L5ProgettoSettimanaleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.S7L5_ProgettoSettimanale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S7L5ProgettoSettimanaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
