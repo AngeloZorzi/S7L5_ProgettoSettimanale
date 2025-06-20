9c939aa9cbf136b429e0f4bcdeada58617098004
AngeloZorzi <astro.drg666@gmail.com>
2025-06-20 11:31:58 +0200

    adds dependencies and creates Role and User

---
+++ src/main/java/it/epicode/S7L5_ProgettoSettimanale/enumerating/Role.java
@@ -0,0 +1,6 @@
+package it.epicode.S7L5_ProgettoSettimanale.enumerating;
+
+public enum Role {
+    USER,
+    ORGANIZER
+}
---
+++ src/main/java/it/epicode/S7L5_ProgettoSettimanale/model/User.java
@@ -0,0 +1,25 @@
+package it.epicode.S7L5_ProgettoSettimanale.model;
+
+import it.epicode.S7L5_ProgettoSettimanale.enumerating.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
